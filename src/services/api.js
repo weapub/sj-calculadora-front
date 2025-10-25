b29e348b344935cf56ef06f32d0dc75ba23b66e0
weapub <weapublicidad@gmail.com>
2025-10-25 17:02:48 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
+const API_URL = import.meta.env.VITE_API_URL || "https://sj-calculadora-backend.onrender.com";
 
 async function handleResponse(res) {
   if (!res.ok) {
